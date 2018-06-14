09acd24bae778d81edbdc17d19a4b4ca064910ec
Adican12 <adican12@gmail.com>
2018-06-14 16:19:04 +0300

    14.6

--- Common/Control.h
+++ Common/Control.h
@@ -17,21 +17,30 @@ protected:
 
 public:
 	Control();
+	// position setter
 	void setTop(short Top) { this->top = Top; };
 	void setLeft(short Left) { this->left = Left; };
 	void setWidth(short Width) { this->width= Width; };
 	void setHeight(short Height) { this->height = Height; };
-
-	void setColor(Color BG, Color FG) { this->bg = BG; this->fg = FG; };
-
+	
+	// position getter
 	virtual	short getLeft() { return left; };
 	virtual	short getTop() { return top; };
 	virtual	short getWidth() { return width; };
 	virtual	short getHeight() { return height; };
 
+	// set Color background and fourground 
+	void setColor(Color BG, Color FG) { this->bg = BG; this->fg = FG; };
+	// Colors getter
+	Color getBackground() { return bg; };
+	Color getFourground() { return fg; };
+
+	// set foucus
+	static void setFocus(Control& control) { static_control = &control; };
+	// get fouces 
 	static Control* getFocus() { return static_control; };
-	static void setFocus(Control& control) {static_control = &control; };
 	
+	// virual function implemnted by every component
 	virtual void draw(Graphics& g, int x, int y, size_t z) {};
 	virtual void mousePressed(int x, int y, bool isLeft) {};
 	virtual void keyDown(int keyCode, char charecter) {};
--- Common/TextBox.cpp
+++ Common/TextBox.cpp
@@ -1,11 +1,8 @@
 #include "TextBox.h"
 
-
-
 TextBox::TextBox()
 {
 }
-
 
 TextBox::~TextBox()
 {
@@ -18,13 +15,54 @@ void TextBox::draw(Graphics& g, int x, int y, size_t z) {
 	
 	g.write(1,1,this->_value);
 	
-	
 }
 
 void TextBox::keyDown(int keyCode, char charecter) {
-	//graphics.moveTo(this->_position.X + 2, this->_position.Y + 2);
-	//this->value = string(1, ch);
-	//graphics.write(string(1, ch));
+	/*
+	if (_value.size() < width - 2) {//check if you can change the content in textbox
+		if (charecter > '\x1F' && charecter < '\x7F') {//check if value is not a navigation key
+			printf("%c", charecter);
+			if (_value.size() == _cursorPosition - 1) {
+				_value += charecter;
+			}
+			else {
+				_value.insert(_cursorPosition, &charecter, 1);
+			}
+			Control::graphics.moveTo(left + _value.size() + 1, top + 1);
+			++_cursorPosition;
+			return;
+		}
+	}
+	switch (keyCode) {
+	case VK_RIGHT: {
+		if (_cursorPosition < _width) {
+			++_cursorPosition;
+			Control::graphics.moveTo(_left - _value.size() - 1, _top + 2);
+		}
+		break;
+	}
+	case VK_LEFT: {
+		if (_cursorPosition > 0) {
+			--_cursorPosition;
+			Control::graphics.moveTo(_left + _value.size() + 1, _top + 2);
+		}
+		break;
+	}
+	case VK_BACK: {
+		if (_cursorPosition  > 0) {
+			--_cursorPosition;
+			_value.erase(_cursorPosition, 1);
+		}
+		break;
+	}
+	case VK_DELETE: {
+		if (_cursorPosition > 0) {
+			_value.erase(_cursorPosition, 1);
+		}
+		break;
+	}
+	}
+	*/
 }
 
 void TextBox::mousePressed(int x, int y, bool isLeft) {

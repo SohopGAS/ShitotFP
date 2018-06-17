37703b7b7418985c2fea615e9d4fae55391c94ef
Adican12 <adican12@gmail.com>
2018-06-17 22:49:04 +0300

    mouseListner added

--- Common/Button.cpp
+++ Common/Button.cpp
@@ -3,51 +3,30 @@
 
 using namespace std;
 
-
-
 Button::Button(string & s)
 {
-	expand_string(s);
-	
-
+	expand_string(s);	
 }
 
 void Button::setValue(string s)
 {
 	expand_string(s);
 	Label::setValue(s);
-
 }
+
+
 
 
 void Button::mousePressed(int x, int y, bool isLeft)
 {
 	CLogger::GetLogger()->Log("mouse pressed");
-	bool mouse_clicked = false;
-	POINT cursorPostion;
-	int mX, mY;
-	cursorPostion.y = y;
-	cursorPostion.x = x;
-	string message = "mouse_cliecked";
-	int    nNum = 10;
-	if (isLeft)
-	{
 
-		mX = cursorPostion.x;
-		mY = cursorPostion.y;
-		GetCursorPos(&cursorPostion);
+//	if (isInside(x, y, _left + panelLeft, _top + panelTop, _width, _height)) {
+	///	for (MouseListener* listener : listeners) {
+		//	listener->MousePressed(x, y, isLeft);
+	//	}
+	//}
 
-		if (GetAsyncKeyState(VK_LBUTTON))
-		{
-			mouse_clicked = true;
-
-
-
-
-		}
-		
-
-	}
 }
 
 
@@ -64,8 +43,14 @@ void Button::draw(Graphics& g, int x, int y, size_t z)
 {
 	CLogger::GetLogger()->Log("DRAW BUTTON");
 	Label::draw(g, x, y, z);
-	
-
-
 }
 
+////////////////////////*
+/*void Button::draw(Graphics g, int left, int top, int layer) {
+
+	if (layer == getZIndex()) {
+		Label::draw(g, _left + (panelLeft = left), _top + (panelTop = top), layer);
+		g.setCursorVisibility(false);
+	}
+}*/
+////////////////////////

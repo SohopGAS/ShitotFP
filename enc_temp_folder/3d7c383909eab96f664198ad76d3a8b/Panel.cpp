5fb558cd20da94a3341a2835f52da24301fdc794
Adican12 <adican12@gmail.com>
2018-06-26 18:37:11 +0300

    before marge from master

---
+++ enc_temp_folder/3d7c383909eab96f664198ad76d3a8b/Panel.cpp
@@ -0,0 +1,36 @@
+#include "Panel.h"
+
+void Panel::mousePressed(int x, int y, bool isLeft)
+{
+	int i;
+	for (i = 0; i < controls.size(); i++) {
+		if( isInside(x,y,controls[i]->getLeft(), controls[i]->getTop(), controls[i]->getWidth(), controls[i]->getHeight() ) );
+		{
+			controls[i]->mousePressed(x, y, isLeft);
+		}
+	}
+}
+ void Panel:: keyDown(int keyCode, char charecter) {};
+
+
+void Panel::Add(Control* control) {
+	controls.push_back(control);
+}
+
+void Panel::draw(Graphics& g, short x, short y, size_t z) {
+	
+		Control::draw(g, x, y, z);
+		for(Control* c : this->controls)
+			c->draw(g, x, y, z);
+	
+}
+
+void Panel::getAllControls(vector<Control*>* controls_)
+{
+	for (Control *c : this->controls) {
+		controls_->push_back(c);
+	}
+}
+
+
+
